f28a169a93e7137e4981c2f07fd0b37bd1236cbe
chiedozie013 <chiedozie013@gmail.com>
2022-09-08 11:04:42 +0000

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,7 @@
 /**
  * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
+ *   followed by a new line, to the stantard error.
  *   Return: Always 0 (success)
  */
 int main(void)
