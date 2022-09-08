9f29b71214abed3007a0e4e1bea033c1ef42f8b3
chiedozie013 <chiedozie013@gmail.com>
2022-09-08 10:40:09 +0000

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the phase with proper grammar,
+ * main - prints the phrase with proper grammar,
  * but the outcome is a piece of art,
  * Return: 0 if exited properly, non-zero otherwise
  */
