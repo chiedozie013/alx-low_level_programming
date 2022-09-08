f658ff8e57f1b91c3e74336258f74bdc5dc3e203
chiedozie013 <chiedozie013@gmail.com>
2022-09-08 10:16:06 +0000

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
-#include <studio.h>
+1	#include <stdio.h>
+2
+3	/**
+4	 * main - prints the phase "Programming is like building a multilingual puzzle
+5	 * Return: 0 if exited properly, non-zero otherwise
+6	 */
+7	int main (void);
+8	{
+9		puts("\"Programming is like building a multilingual puzzle");
+10		return (0);
+11	}
 
-/**
- * main - prints the phase "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
- */
-
-int main (void);
-{
-	
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-}
-	
