999882901ae754eb77782cf399ba72bae3858180
chiedozie013 <chiedozie013@gmail.com>
2022-09-13 13:11:49 +0000

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,8 @@
  * @c: the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
  */
-int int _isalpha(int c)
+int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	_purchar (0);
 }
