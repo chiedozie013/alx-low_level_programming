645190bfb2c1a83166d66e8339abee83337919c1
chiedozie013 <chiedozie013@gmail.com>
2022-09-13 18:38:45 +0000

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,8 +2,8 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @n: The int to extract the last digit from
- * Return: Value of the last digit
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
  */
 int print_last_digit(int n)
 {
