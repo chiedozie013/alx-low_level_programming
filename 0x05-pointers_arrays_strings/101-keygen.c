63f51c346242dbe0db4a0e52713a12aac4c97338
chiedozie013 <chiedozie013@gmail.com>
2022-09-19 12:25:47 +0000

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -15,14 +15,14 @@ int main(void)
 
 	sum = 0;
 
-	strand(time(NULL));
+	srand(time(NULL));
 
 	for (i = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
-		if ((2772 - sum) = '0' < 78)
+		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
 			sum += n;
