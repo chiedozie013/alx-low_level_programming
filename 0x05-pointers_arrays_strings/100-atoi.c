5b6bfb22a0bdb5bdd6d1ef8be15ba513a937e937
chiedozie013 <chiedozie013@gmail.com>
2022-09-19 12:51:04 +0000

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,7 +4,7 @@
  * _atoi -  converts a string to an integer
  * @s: string to be converted
  *
- * Return the int converted from the string
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
