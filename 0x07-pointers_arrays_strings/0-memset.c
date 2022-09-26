f75c00230c71a277aefa895f4b102fb616a2a8dc
chiedozie013 <chiedozie013@gmail.com>
2022-09-26 12:17:28 +0000

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
  * @b: char to copy
  * @n: number of times to copy b
  *
- * Retuen: pointer to the memory area s
+ * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
