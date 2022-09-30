c7400a70740e9dfdda342592689e7883c45d41e7
chiedozie013 <chiedozie013@gmail.com>
2022-09-30 10:46:09 +0000

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - entry
+ * @argv : the string from the terminal
+ * @argc : the number of arameters
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int mul;
+
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	return (0);
+}
