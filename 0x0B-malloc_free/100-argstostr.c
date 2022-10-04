2dff2dfb1d25340b501f58cf4399c32cba707a71
chiedozie013 <chiedozie013@gmail.com>
2022-10-04 09:33:30 +0000

    Task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -18,8 +18,9 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av [i][j] != '\0'; j++)
+		for (j = 0; av[i][j] != '\0'; j++)
 			len++;
+		len++
 	}
 
 	str = malloc(sizeof(char) * (len + 1));
