4b6d1fb0b829374c4460e0444b750f4514a6da4d
chiedozie013 <chiedozie013@gmail.com>
2022-10-06 12:00:14 +0000

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,9 +30,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *p;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	if (n > len2)
